1b3cf0f4344864144adfe6baf183c163a1cf29a3
lebalex <lebalex@mail.ru>
2018-11-28 10:44:43 +0300

    Вырузка чеков бенет

---
+++ Benet/auto.bat
@@ -0,0 +1,3 @@
+﻿
+rem Start "C:\Program Files (x86)\1cv8\8.3.12.1440\bin\1cv8.exe" /@"d:\run"
+"C:\Program Files (x86)\1cv8\8.3.12.1440\bin\1cv8.exe" /F "D:\Abram\1c_roz" /DisableStartupMessages /Cauto /Execute"D:\Abram\dev\Abram_1c\Benet\Benet_20181128.epf"
